127521f2f3aed4960c957efe6b354d7857d90c86
clorkapunk <temir225531@gmail.com>
2024-10-07 18:56:37 +0500

    cors fix

--- server/app.js
+++ server/app.js
@@ -14,9 +14,10 @@ app.use(logger('dev'));
 app.use(cookieParser())
 app.use(cors({
     origin: [
-        "http://localhost:5173",
-        "http://localhost:4173",
-        "http://127.0.0.1:5173"
+        process.env.CLIENT_URL
+        // "http://localhost:5173",
+        // "http://localhost:4173",
+        // "http://127.0.0.1:5173"
     ],
     credentials: true
 }))
