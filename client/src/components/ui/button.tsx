7b95f2df98e1e36dce724ae42cb75103927f4c35
clorkapunk <temir225531@gmail.com>
2024-10-20 23:56:29 +0500

    styled

--- client/src/components/ui/button.tsx
+++ client/src/components/ui/button.tsx
@@ -18,6 +18,7 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
+          dark: 'bg-zinc-800 text-zinc-200 border-none hover:bg-zinc-600 hover:text-zinc-100'
       },
       size: {
         default: "h-10 px-4 py-2",
